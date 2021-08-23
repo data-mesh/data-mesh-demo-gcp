f6403ef8b40f0e02547141d92272078b3e515481
samiarahman <samia.rahman.lsu@gmail.com>
2021-08-23 18:18:47 -0500

    Provision infrastructure by generating cicd pipeline (#8)
    * Generate pipeline for terraform plan and apply per new or changed data product folder 
    
    Co-authored-by: Samia Rahman <srahman@thoughtworks.com>

---
+++ .scripts/generate-pipeline-config.py
@@ -0,0 +1,58 @@
+from jinja2 import Template
+import subprocess
+
+# Get the file changes in data-infrastructure/data-products
+changes = subprocess.run(
+    ['git', 'diff-tree', '-r', '--no-commit-id', '--name-status', 'HEAD', '../data-infrastructure/data-products'],
+    check=True,
+    capture_output=True
+).stdout.decode('utf-8').splitlines()
+
+print('Printing changes {}'.format(changes))
+
+# Get data products to provision
+def get_data_products_configs(gitChanges):
+    configs = dict()
+    for gitChange in gitChanges: 
+        gitChangeArray = gitChange.split('\t')
+        print('Print diff split {}'.format(gitChangeArray))
+
+        if gitChangeArray[0].startswith('M') | gitChangeArray[0].startswith('A'):
+                print('file path that changed or added {}'.format(gitChangeArray[1]))
+                data_product_dir=gitChangeArray[1].split('/')[2]
+                configValue = {
+                        "name": data_product_dir,
+                        "dir": 'data-infrastructure/data-products/{}'.format(data_product_dir)
+                    }
+                configKey = gitChangeArray[1].split('/')[2]
+                configs[configKey] = configValue
+    return configs
+
+data_product_configs = get_data_products_configs(changes)
+print('Printing data products configs {}'.format(data_product_configs))
+
+if len(data_product_configs) > 0:
+    # Create the data for rendering the template
+    template_data = {
+        "data_products": list(data_product_configs.values())
+    }
+    print("Printing the template data {}".format(template_data))
+
+    # Read the template file 
+    file = open('./templates/provision-data-product.yml',mode='r')
+    template = file.read()
+    file.close()
+
+    # Generate the pipeline config
+    j2_template = Template(template)
+    output=j2_template.render(template_data)
+
+    with open('../configs/generated_config.yml', 'w') as f:
+        f.write(output)
+
+else:
+    default_pipeline = ""
+    with open('./templates/default-do-nothing.yml',mode='r') as f:
+        default_pipeline = f.read()
+    with open('../configs/generated_config.yml', 'w') as f:
+        f.write(default_pipeline)
