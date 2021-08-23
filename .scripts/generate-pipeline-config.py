91b201bd9dbaa4d5a203d10a63601a190eb5c434
Samia Rahman <srahman@thoughtworks.com>
2021-08-23 16:08:17 -0500

    Detect file changes from git diff and then generate pipeline code

--- .scripts/generate-pipeline-config.py
+++ .scripts/generate-pipeline-config.py
@@ -1,59 +1,39 @@
 from jinja2 import Template
-
 import subprocess
 
 # Get the file changes in data-infrastructure/data-products
-
-# head = subprocess.run(
-#                           ['git', 'rev-parse', 'HEAD'],
-#                           check=True,
-#                           capture_output=True
-#                         ).stdout.decode('utf-8').strip()
-
-# base = subprocess.run(
-#                           ['git', 'rev-parse', 'HEAD~1'], 
-#                           check=True,
-#                           capture_output=True
-#                         ).stdout.decode('utf-8').strip()
-
-# print('Comparing {}...{}'.format(base, head))
-
-
 changes = subprocess.run(
     ['git', 'diff-tree', '-r', '--no-commit-id', '--name-status', 'HEAD', '../data-infrastructure/data-products'],
     check=True,
     capture_output=True
 ).stdout.decode('utf-8').splitlines()
 
-changes=['M\t.data-infrastructure/data-products/data-product-a/main.tf', 'M\t.data-infrastructure/data-products/data-product-b/main.tf']
-
 print('Printing changes {}'.format(changes))
 
 # Get data products to provision
-def get_data_products_folders(diffs):
-    for diff in diffs: 
-        print('Print diff split'.format(diff.split("\t")))
-        return [] 
+def get_data_products_configs(gitChanges):
+    configs = list()
+    for gitChange in gitChanges: 
+        gitChangeArray = gitChange.split('\t')
+        print('Print diff split {}'.format(gitChangeArray))
 
-data_products_folders = get_data_products_folders(changes)
+        if gitChangeArray[0].startswith('M') | gitChangeArray[0].startswith('A'):
+                print('file path that changed or added {}'.format(gitChangeArray[1]))
+                configs.append(
+                    {
+                        "name": gitChangeArray[1].split('/')[2],
+                        "dir": gitChangeArray[1]
+                    })
+    return configs
 
-print('Printing data products folders {}'.format(data_products_folders))
+data_product_configs = get_data_products_configs(changes)
+print('Printing data products configs {}'.format(data_product_configs))
 
 # Create the data for rendering the template
-## TODO
-data = {
-    "data_products": [
-        {
-            "name": "data-product-a",
-            "dir": "data-infrastructure/data-products/data-product-a"
-        }, 
-        {   
-            "name": "data-product-b", 
-            "dir": "data-infrastructure/data-products/data-product-b"
-        }],
+template_data = {
+    "data_products": data_product_configs
 }
-
-print(data)
+print("Printing the template data {}".format(template_data))
 
 # Read the template file 
 # Open a file: file
@@ -65,7 +45,7 @@ file.close()
 
 # Generate the pipeline config
 j2_template = Template(template)
-output=j2_template.render(data)
+output=j2_template.render(template_data)
 
 with open('../configs/generated_config.yml', 'w') as f:
     f.write(output)
