d46a0e0b78735cb5893aa449726aa565e7844881
Samia Rahman <srahman@thoughtworks.com>
2021-08-22 23:32:41 -0500

    Test 2

---
+++ data-infrastructure/data-products/data-product-a copy/main.tf
@@ -0,0 +1,38 @@
+module "data_product" {
+  source   = "../../modules/data-product"
+  data_product_name ="dataproducta"
+  inputs=[
+    {
+      "name"="storage-a-1"
+      "input_type"="Storage"
+    }
+  
+  ]
+  outputs=[
+    {
+      "name"="dataset1"
+      "output_type"="SQL"
+      consumer_email="data-product-a-consumers@thoughtworks.com"
+    },
+    {
+      "name"="storage-a-1"
+      "output_type"="Storage"
+      consumer_email="data-product-a-consumers@thoughtworks.com"
+    },
+    ]
+  compute=true
+  project_name= var.project_name
+  project_id = var.project_id
+}
+
+output "data_flow_bucket" {
+  value=module.data_product.data_flow_bucket
+}
+
+output "outputs" {
+  value = module.data_product.outputs_addresses
+}
+
+output "inputs" {
+  value = module.data_product.inputs_addresses
+}
