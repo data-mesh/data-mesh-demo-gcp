fb0ea75070172f030f879e41020986586c354dd1
Samia Rahman <srahman@thoughtworks.com>
2021-08-24 15:20:36 -0500

    Provision data product d

---
+++ data-infrastructure/data-products/data-product-d/main.tf
@@ -0,0 +1,33 @@
+module "data_product" {
+  source   = "../../modules/data-product"
+  data_product_name ="dataproductd"
+  inputs=[
+  ]
+  outputs=[
+    {
+      "name"="dataset1"
+      "output_type"="SQL"
+      consumer_email="data-product-d-consumers@thoughtworks.com"
+    },
+    {
+      "name"="storage_b_1"
+      "output_type"="Storage"
+      consumer_email="data-product-d-consumers@thoughtworks.com"
+    },
+    ]
+  compute=true
+  project_name= var.project_name
+  project_id = var.project_id
+}
+
+output "data_flow_bucket" {
+  value=module.data_product.data_flow_bucket
+}
+
+output "outputs" {
+  value = module.data_product.outputs_addresses
+}
+
+output "inputs" {
+  value = module.data_product.inputs_addresses
+}
