9a6c16679867aafd5a8f9c0dcd0bc53dc3a8ec52
Samia Rahman <srahman@thoughtworks.com>
2021-08-24 22:54:49 -0500

    Restructure code for presentation

--- infra_base/main.tf
+++ infra_base/main.tf
@@ -1,3 +1,5 @@
+# Base infrastructure 
+
 resource "google_project_service" "service" {
   service = "iam.googleapis.com" 
 
@@ -5,86 +7,32 @@ resource "google_project_service" "service" {
   disable_on_destroy = false
 }
 
-# DP1 Service Account
+# Default VPC network for dataflow firewall rull
+resource "google_compute_firewall" "default" {
+  name    = "dataflow-traffic"
+  network = google_compute_network.default.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["12345-12346"]
+  }
+
+  target_tags = ["dataflow"]
+}
+
+# Allow ingress of dataflow
+resource "google_compute_network" "default" {
+  name = "default"
+}
+
+# -------Data Product A ---------------
+# DPA Service Account
 resource "google_service_account" "service_account-dp-a" {
   account_id   = "dp-a-sa"
   display_name = "Service Account for data product A"
 }
 
-# DP2 Service account
-resource "google_service_account" "service_account-dp-b" {
-  account_id   = "dp-b-sa"
-  display_name = "Service Account for data product B"
-}
-
-# DP1 Output port
-resource "google_bigquery_dataset" "dataset-dp-a" {
-  dataset_id                  = "dp1ds"
-  friendly_name               = "dp1-dataset"
-  location                    = "US"
-  default_table_expiration_ms = 3600000
-  default_partition_expiration_ms = 5184000000
-  delete_contents_on_destroy = true
-
-  access {
-    role          = "OWNER"
-    user_by_email = google_service_account.service_account-dp-a.email
-  }
-
-  # TODO Refactor this such that dataset-dp-a has a role binding to access group dp-a-consumers, such that data product b
-  access {
-    role = "READER"
-    user_by_email = google_service_account.service_account-dp-b.email
-  }
-}
-
-resource "random_string" "random_dp_id" {
-  length           = 16
-  special          = false
-  upper = false
-}
-
-resource "google_storage_bucket" "dp-a-output-sb" {
-  name          = "dp-a-output-${random_string.random_dp_id.id}"
-  location      = "US"
-  force_destroy = true
-}
-
-
-# DP1 Input port
-resource "google_storage_bucket" "dp-a-sb" {
-  name          = "dp-a-input-${random_string.random_dp_id.id}"
-  location      = "US"
-  force_destroy = true
-}
-
-# DP1 SP permissions
-data "google_iam_policy" "dp-a-admin" {
-  binding {
-    role = "roles/storage.objectCreator"
-    members = [
-      "serviceAccount:${google_service_account.service_account-dp-a.email}",
-    ]
-  }
-
-  binding {
-    role = "roles/storage.admin"
-    members = [
-      "serviceAccount:data-mesh-base-infra-provision@${var.project_name}.iam.gserviceaccount.com",
-      "serviceAccount:${google_service_account.service_account-dp-a.email}",
-    ]
-  }
-}
-
-resource "google_storage_bucket_iam_policy" "dp-a-sb-policy" {
-  bucket = google_storage_bucket.dp-a-sb.name
-  policy_data = data.google_iam_policy.dp-a-admin.policy_data
-}
-
-resource "google_storage_bucket_iam_policy" "dp-a-output-sb-policy" {
-  bucket = google_storage_bucket.dp-a-output-sb.name
-  policy_data = data.google_iam_policy.dp-a-admin.policy_data
-}
+# DPA Compute
 
 data "google_iam_policy" "dp-a-temp-sb-policy" {
   binding {
@@ -135,19 +83,100 @@ resource "google_project_iam_member" "dp-a-sa-compute-viewer" {
   member  = "serviceAccount:${google_service_account.service_account-dp-a.email}"
 }
 
-resource "google_project_iam_member" "dp-b-sa-bq-job-user" {
-  role    = "roles/bigquery.jobUser"
-  member  = "serviceAccount:${google_service_account.service_account-dp-b.email}"
-}
-
-# DP1 internals
 resource "google_storage_bucket" "dp-a-dataflow-temp" {
   name          = "dp-a-df-temp-${random_string.random_dp_id.id}"
   location      = "US"
   force_destroy = true
 }
 
-# DP2 output port
+# DPA Storage bucket policy
+data "google_iam_policy" "dp-a-admin" {
+  binding {
+    role = "roles/storage.objectCreator"
+    members = [
+      "serviceAccount:${google_service_account.service_account-dp-a.email}",
+    ]
+  }
+
+  binding {
+    role = "roles/storage.admin"
+    members = [
+      "serviceAccount:data-mesh-base-infra-provision@${var.project_name}.iam.gserviceaccount.com",
+      "serviceAccount:${google_service_account.service_account-dp-a.email}",
+    ]
+  }
+}
+
+# DPA Input Port
+resource "google_storage_bucket" "dp-a-sb" {
+  name          = "dp-a-input-${random_string.random_dp_id.id}"
+  location      = "US"
+  force_destroy = true
+}
+
+resource "google_storage_bucket_iam_policy" "dp-a-sb-policy" {
+  bucket = google_storage_bucket.dp-a-sb.name
+  policy_data = data.google_iam_policy.dp-a-admin.policy_data
+}
+
+# DPA Output
+
+## DPA Bigquery Dataset
+resource "google_bigquery_dataset" "dataset-dp-a" {
+  dataset_id                  = "dp1ds"
+  friendly_name               = "dp1-dataset"
+  location                    = "US"
+  default_table_expiration_ms = 3600000
+  default_partition_expiration_ms = 5184000000
+  delete_contents_on_destroy = true
+
+  access {
+    role          = "OWNER"
+    user_by_email = google_service_account.service_account-dp-a.email
+  }
+
+  # TODO Refactor this such that dataset-dp-a has a role binding to access group dp-a-consumers, such that data product b
+  access {
+    role = "READER"
+    user_by_email = google_service_account.service_account-dp-b.email
+  }
+}
+
+resource "random_string" "random_dp_id" {
+  length           = 16
+  special          = false
+  upper = false
+}
+
+## DPA Output Storage Bucket
+resource "google_storage_bucket" "dp-a-output-sb" {
+  name          = "dp-a-output-${random_string.random_dp_id.id}"
+  location      = "US"
+  force_destroy = true
+}
+
+resource "google_storage_bucket_iam_policy" "dp-a-output-sb-policy" {
+  bucket = google_storage_bucket.dp-a-output-sb.name
+  policy_data = data.google_iam_policy.dp-a-admin.policy_data
+}
+
+# -------Data Product B---------------
+
+# DPB Service account
+resource "google_service_account" "service_account-dp-b" {
+  account_id   = "dp-b-sa"
+  display_name = "Service Account for data product B"
+}
+
+# DPB Compute
+resource "google_project_iam_member" "dp-b-sa-bq-job-user" {
+  role    = "roles/bigquery.jobUser"
+  member  = "serviceAccount:${google_service_account.service_account-dp-b.email}"
+}
+
+# DPB Outputs
+
+## DPB Output Bigquery dataet
 resource "google_bigquery_dataset" "dataset-dp-b" {
   dataset_id                  = "dp2ds"
   friendly_name               = "dp2-dataset"
@@ -159,27 +188,4 @@ resource "google_bigquery_dataset" "dataset-dp-b" {
     role          = "OWNER"
     user_by_email = google_service_account.service_account-dp-b.email
   }
-}
-
-# Default VPC network for dataflow firewall rull
-resource "google_compute_firewall" "default" {
-  name    = "dataflow-traffic"
-  network = google_compute_network.default.name
-
-  allow {
-    protocol = "tcp"
-    ports    = ["12345-12346"]
-  }
-
-  target_tags = ["dataflow"]
-}
-
-# Allow ingress of dataflow
-resource "google_compute_network" "default" {
-  name = "default"
-}
-
-output "dp-a-uid" {
-  # This syntax is for Terraform 0.12 or later.
-  value = random_string.random_dp_id.id
 }
